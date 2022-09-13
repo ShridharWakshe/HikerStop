58901e9b7f3b1fdce2192b9c7d745062318242e4
Yogeshwari-Bulbule <yogeshwaribulbule@gmail.com>
2022-09-13 12:29:32 +0530

    firstProject

---
+++ demo/src/main/java/com/cdac/demo/HikerstopApplication.java
@@ -0,0 +1,13 @@
+package com.cdac.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikerstopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikerstopApplication.class, args);
+	}
+
+}
