8fae53e8f2841e446b60dc01bf0bfa3da66ada44
Shridhar <shridharwakshe12@gmail.com>
2022-09-14 13:53:36 +0530

    temp file

---
+++ hikerstop/src/test/java/com/cdac/hikerstop/HikerstopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdac.hikerstop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikerstopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
