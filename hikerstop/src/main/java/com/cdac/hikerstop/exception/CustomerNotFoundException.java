c10dde6363b6588a8fdc9da6903c4017c4caad7e
BhushanPatilJH <patilbhushan971@gmail.com>
2022-09-16 00:34:03 +0530

    customer dto, servicesand contoller removed

--- hikerstop/src/main/java/com/cdac/hikerstop/exception/CustomerNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.cdac.hikerstop.exception;
-
-@SuppressWarnings("serial")
-public class CustomerNotFoundException extends RuntimeException{
-	
-	public CustomerNotFoundException(String message) {
-		super(message);
-	}
-}
