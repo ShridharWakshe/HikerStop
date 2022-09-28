c9690fc325c28d67f3ee36eadc0c238a6ae47231
kumrawatdipak <101056295+kumrawatdipak@users.noreply.github.com>
2022-09-28 19:40:58 +0530

    update application

--- HikerStopBackend/src/main/java/com/app/HikerStopBackend.java
+++ HikerStopBackend/src/main/java/com/app/HikerStopBackend.java
@@ -8,10 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-//@SpringBootApplication
 
-@EnableJpaAuditing
-@EnableEncryptableProperties
 public class HikerStopBackend {
 
 	public static void main(String[] args) {
