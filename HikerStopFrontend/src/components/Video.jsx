c0a5c38f7dfb166134490a4dfdc1b60c6d74626a
Shridhar <shridharwakshe12@gmail.com>
2022-09-29 12:21:25 +0530

    video

--- HikerStopFrontend/src/components/Video.jsx
+++ HikerStopFrontend/src/components/Video.jsx
@@ -4,7 +4,7 @@ import video2 from "../components/video2.mp4"
 
 const Video = () =>{
     return(
-        <div className="main bg-white">
+        <div className="main bg-white"style={{height:"570px"}}>
         <video src={video2} autoPlay loop muted/>
         <div className="content">
         <h1>The Mountains Are Calling...</h1>
